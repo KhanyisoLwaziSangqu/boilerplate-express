d6eee04afd8a75d2483f532d7e1aa8e31c2b93df
Khanyiso <khanyiso.lwazi.sangqu@gmail.com>
2025-05-05 09:33:26 +0200

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ let app = express();
 
 console.log("Hello World");
 
+// Serve static assets from the "public" folder
+app.use("/public", express.static(__dirname + "/public"));
+
 // Serve index.html at the root route
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
